f7c1cef4429fb91dad08c39f201ba4c60ecde5b1
Khaula Zumar <khaulazumar15@gmail.com>
2026-09-15 18:33:52 +0530

    Update childLogicParser.js

--- server/childLogicParser.js
+++ server/childLogicParser.js
@@ -287,10 +287,11 @@ function analyzeChildRequest(inputText) {
     secondaryColor = '#2980B9';
   }
   // O. Universal Arbitrary Child Logic Parser
-  const stopWords = new Set(['i', 'want', 'a', 'an', 'the', 'to', 'be', 'in', 'my', 'on', 'me', 'please', 'make', 'give', 'can', 'have', 'and', 'with', 'it']);
+const words = text.replace(/[^a-zA-Z0-9\s]/g, '').split(/\s+/).filter(Boolean);
+
+const stopWords = new Set(['i', 'want', 'a', 'an', 'the', 'to', 'be', 'in', 'my', 'on', 'me', 'please', 'make', 'give', 'can', 'have', 'and', 'with', 'it']);
 
 const meaningful = words.filter(w => !stopWords.has(w.toLowerCase()));
-
     actor = meaningful[0] || 'imagined_hero';
     target = meaningful[1] || 'impossible_wonder';
     action = 'manifest_marvel';
